288f3bf699c9da1649fc0a7c10f1b10f27b891d2
PAPPI-HS <pappihs@gmail.com>
2021-09-09 18:11:05 +0500

    thu sep 9, 6:10 PM

---
+++ javasc.js
@@ -0,0 +1,103 @@
+'use strict;'
+
+// const game = {
+//     team1: 'Bayern Munich',
+//     team2: 'Borrussia Dortmund',
+//     players: [
+//     [
+//     'Neuer',
+//     'Pavard',
+//     'Martinez',
+//     'Alaba',
+//     'Davies',
+//     'Kimmich',
+//     'Goretzka',
+//     'Coman',
+//     'Muller',
+//     'Gnarby',
+//     'Lewandowski',
+//     ],
+//     [
+//     'Burki',
+//     'Schulz',
+//     'Hummels',
+//     'Akanji',
+//     'Hakimi',
+//     'Weigl',
+//     'Witsel',
+//     'Hazard',
+//     'Brandt',
+//     'Sancho',
+//     'Gotze',
+//     ],
+//     ],
+//     score: '4:0',
+//     scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+//     'Hummels'],
+//     date: 'Nov 9th, 2037',
+//     odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//     },
+// };
+
+
+
+// let scored = {}
+// const jonas = Object.values(game.scored);
+
+// console.log(jonas.count)
+// for (const x of Object.values(game.scored) ){
+//     let a = jonas.count(x);
+//     scored[x] = a;
+
+//}
+
+// scored['hassan']++;
+// console.log(scored)
+
+
+//const players = Object.entries(game.scored);
+//console.log(typeof players[0][0]);
+
+
+// for (const [goal,player] of game.scored.entries()){
+//      console.log(`Goal ${goal+1} : ${player}`)
+// }
+
+// const odd = Object.values(game.odds);
+// // console.log(odd);
+
+// let sum = 0;
+// let average=0;
+
+// for (const a of odd){
+//     sum += a;
+// }
+
+// average = sum/odd.length;
+// console.log(average);
+
+// for(let [name,od] of Object.entries(game.odds)) {
+//     name === 'x' ? console.log(`odds of draw:${od}`):console.log(`odds of victory ${game[name]}:${od}`)
+// }
+//for (const [a,b] of game.odds.entries())
+
+
+
+
+// console.log(scorers)
+
+//const {team1:a,x:b,team2:c} = game.odds ;
+//console.log((a+b+c)/3)
+
+// var a = ['a', 'b', 'c'];
+// var iterator = a.entries();
+
+// for (let [a,b] of iterator) {
+//   console.log(a,b);
+// }
+// [0, 'a']
+// [1, 'b']
+// [2, 'c']
